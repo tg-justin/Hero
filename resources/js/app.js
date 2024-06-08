88576e1fd07ef4f0861930b877d29c1f4979ede0
Justin Nauman <justin@tabletopgaymers.org>
2024-06-08 09:34:32 -0500

    Update app.js
    Missing imports for quill

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
 
+import 'quill/dist/quill.snow.css';
+import 'quill/dist/quill.bubble.css';
+import 'quill/dist/quill.core.css';
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
