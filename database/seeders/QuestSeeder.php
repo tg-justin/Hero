966eb4a26ac4353723293cf936eb02f220f0623a
Justin Nauman <justin@tabletopgaymers.org>
2024-06-08 10:46:00 -0500

    Update QuestSeeder.php

--- database/seeders/QuestSeeder.php
+++ database/seeders/QuestSeeder.php
@@ -15,9 +15,9 @@ class QuestSeeder extends Seeder
             [
                 'user_id' => 1, // Assuming the admin user has an ID of 1
                 'title' => 'Complete Your Hero Registration',
-                'summary' => 'Tell us a bit about yourself and your interests!',
-                'description' => '',
-                'points' => 10,
+                'intro_text' => 'Tell us a bit about yourself and your interests!',
+                'directions_text' => '',
+                'xp' => 10,
                 'category_id' => 1,
                 'min_level' => 0,
                 'repeatable' => 0,
@@ -26,9 +26,9 @@ class QuestSeeder extends Seeder
             [
                 'user_id' => 1, // Assuming the admin user has an ID of 1
                 'title' => 'Attend a Pride event wearing gaming gear',
-                'summary' => 'Show your pride and your love for gaming!',
-                'description' => '',
-                'points' => 50,
+                'intro_text' => 'Show your pride and your love for gaming!',
+                'directions_text' => '',
+                'xp' => 50,
                 'category_id' => 2,
                 'min_level' => 1,
                 'repeatable' => 1,
@@ -37,9 +37,9 @@ class QuestSeeder extends Seeder
             [
                 'user_id' => 1, // Assuming the admin user has an ID of 1
                 'title' => 'Give TG a bump on social media',
-                'summary' => 'Help spread the word about Tabletop Gaymers!',
-                'description' => '',
-                'points' => 10,
+                'intro_text' => 'Help spread the word about Tabletop Gaymers!',
+                'directions_text' => '',
+                'xp' => 10,
                 'category_id' => 3,
                 'min_level' => 1,
                 'repeatable' => 1,
@@ -48,9 +48,9 @@ class QuestSeeder extends Seeder
             [
                 'user_id' => 1, // Assuming the admin user has an ID of 1
                 'title' => 'Host a Birthday Fundraiser on Facebook',
-                'summary' => 'Raise funds for Tabletop Gaymers on your birthday!',
-                'description' => '',
-                'points' => 50,
+                'intro_text' => 'Raise funds for Tabletop Gaymers on your birthday!',
+                'directions_text' => '',
+                'xp' => 50,
                 'category_id' => 5,
                 'min_level' => 1,
                 'repeatable' => 1,
@@ -59,9 +59,9 @@ class QuestSeeder extends Seeder
             [
                 'user_id' => 1, // Assuming the admin user has an ID of 1
                 'title' => 'Sign-Up to be a Gen Con GM',
-                'summary' => 'Share your love of RPGs at Gen Con!',
-                'description' => '',
-                'points' => 100,
+                'intro_text' => 'Share your love of RPGs at Gen Con!',
+                'directions_text' => '',
+                'xp' => 100,
                 'category_id' => 5,
                 'min_level' => 1,
                 'repeatable' => 0,
@@ -70,9 +70,9 @@ class QuestSeeder extends Seeder
             [
                 'user_id' => 1, // Assuming the admin user has an ID of 1
                 'title' => 'Create T-Shirt Design for the Donor Rewards Website',
-                'summary' => 'Design a cool T-shirt for our donors!',
-                'description' => '',
-                'points' => 100,
+                'intro_text' => 'Design a cool T-shirt for our donors!',
+                'directions_text' => '',
+                'xp' => 100,
                 'category_id' => 3,
                 'min_level' => 2,
                 'repeatable' => 0,
@@ -81,9 +81,9 @@ class QuestSeeder extends Seeder
             [
                 'user_id' => 1, // Assuming the admin user has an ID of 1
                 'title' => 'Refer your local community center to the Gayme Night program',
-                'summary' => 'Help create a safe space for LGBTQ+ gamers!',
-                'description' => '',
-                'points' => 200,
+                'intro_text' => 'Help create a safe space for LGBTQ+ gamers!',
+                'directions_text' => '',
+                'xp' => 200,
                 'category_id' => 2,
                 'min_level' => 2,
                 'repeatable' => 0,
@@ -92,9 +92,9 @@ class QuestSeeder extends Seeder
             [
                 'user_id' => 1, // Assuming the admin user has an ID of 1
                 'title' => 'Refer your local game store to the Gaming Safe Space program',
-                'summary' => 'Make gaming more inclusive!',
-                'description' => '',
-                'points' => 200,
+                'intro_text' => 'Make gaming more inclusive!',
+                'directions_text' => '',
+                'xp' => 200,
                 'category_id' => 2,
                 'min_level' => 2,
                 'repeatable' => 0,
@@ -103,9 +103,9 @@ class QuestSeeder extends Seeder
             [
                 'user_id' => 1, // Assuming the admin user has an ID of 1
                 'title' => 'Write a guide on starting a local gaymer group',
-                'summary' => 'Share your expertise and empower others!',
-                'description' => '',
-                'points' => 300,
+                'intro_text' => 'Share your expertise and empower others!',
+                'directions_text' => '',
+                'xp' => 300,
                 'category_id' => 3,
                 'min_level' => 2,
                 'repeatable' => 0,
