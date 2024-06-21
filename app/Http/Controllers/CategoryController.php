ad02b58931a27bc60b56ec4f553877b97c33dad2
Justin Nauman <justin@tabletopgaymers.org>
2024-06-20 20:52:51 -0500

    Added expected function return types to controller functions

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,18 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
-	public function index()
+	public function index(): View
 	{
 		$categories = Category::paginate(15); // 15 items per page
 
 		return view('categories.index', compact('categories'));
 	}
 
-	public function store(Request $request)
+	public function store(Request $request): RedirectResponse
 	{
 		$validatedData = $request->validate([
 			'name' => 'required|unique:categories|max:255',
@@ -26,17 +28,17 @@ class CategoryController extends Controller
 		return redirect()->route('categories.index')->with('success', 'Category created successfully.');
 	}
 
-	public function create()
+	public function create(): View
 	{
 		return view('categories.create');
 	}
 
-	public function edit(Category $category)
+	public function edit(Category $category): View
 	{
 		return view('categories.edit', compact('category'));
 	}
 
-	public function update(Request $request, Category $category)
+	public function update(Request $request, Category $category): RedirectResponse
 	{
 		$validatedData = $request->validate([
 			'name' => 'required|unique:categories,name,' . $category->id . '|max:255',
@@ -48,7 +50,7 @@ class CategoryController extends Controller
 		return redirect()->route('categories.index')->with('success', 'Category updated successfully.');
 	}
 
-	public function destroy(Category $category)
+	public function destroy(Category $category): RedirectResponse
 	{
 		$category->delete();
 		return redirect()->route('categories.index')->with('success', 'Category deleted successfully.');
