59a3d7c23333cda67de3e5ba25fdd6d27fc8954d
Justin Nauman <justin@tabletopgaymers.org>
2024-06-20 22:00:56 -0500

    Removed shadow after hero profile block, updated /profile forms to match site styles

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,29 +1,29 @@
 <section class="space-y-6">
 	<header>
-		<h2 class="text-lg font-medium text-gray-100">
+		<h2 class="text-lg font-medium ">
 			{{ __('Delete Account') }}
 		</h2>
 
-		<p class="mt-1 text-sm text-gray-400">
+		<p class="mt-1 text-sm">
 			{{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
 		</p>
 	</header>
 
-	<x-danger-button
+	<x-primary-button
 		x-data=""
 		x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-	>{{ __('Delete Account') }}</x-danger-button>
+	>{{ __('Delete Account') }}</x-primary-button>
 
 	<x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
 		<form method="post" action="{{ route('profile.destroy') }}" class="p-6">
 			@csrf
 			@method('delete')
 
-			<h2 class="text-lg font-medium text-gray-100">
+			<h2 class="text-lg font-medium">
 				{{ __('Are you sure you want to delete your account?') }}
 			</h2>
 
-			<p class="mt-1 text-sm text-gray-400">
+			<p class="mt-1 text-sm">
 				{{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
 			</p>
 
@@ -46,9 +46,9 @@
 					{{ __('Cancel') }}
 				</x-secondary-button>
 
-				<x-danger-button class="ms-3">
+				<x-primary-button class="ms-3">
 					{{ __('Delete Account') }}
-				</x-danger-button>
+				</x-primary-button>
 			</div>
 		</form>
 	</x-modal>
