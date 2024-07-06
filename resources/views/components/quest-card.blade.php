29ac48ca9c5bac62d3557c1b26d50fc803f6e8cb
tg-justin <justin@tabletopgaymers.org>
2024-07-06 11:58:06 -0500

    Show total and xp bonus on quest cards

--- resources/views/components/quest-card.blade.php
+++ resources/views/components/quest-card.blade.php
@@ -7,8 +7,13 @@
 		<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 			<polygon points="12 2 19 21 12 17 5 21 12 2"></polygon>
 		</svg>
+		<span class="text-sm text-seance-700">
+			{{ $questLog->xp_awarded + $questLog->xp_bonus }} xp
+			@if($questLog->xp_bonus > 0)
+				({{ $questLog->xp_awarded }} + {{ $questLog->xp_bonus }} bonus xp)
+			@endif
+		</span>
 
-		<span class="text-sm text-seance-700">{{ $quest->xp }} xp</span>
 	</div>
 
 	<div class="mt-4 flex justify-between">
