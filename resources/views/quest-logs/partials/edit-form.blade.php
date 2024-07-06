77e781a9ae1a3fd57bffd4e770743b09e437a2e2
Jeff <jeff@tabletopgaymers.org>
2024-07-06 01:42:46 -0500

    merged changes, re-added min="0" for xp fields.

--- resources/views/quest-logs/partials/edit-form.blade.php
+++ resources/views/quest-logs/partials/edit-form.blade.php
@@ -18,13 +18,13 @@
 
 			<div class="mb-4">
 				<x-input-label for="xp_awarded" class="text-gray-700" :value="__('XP Awarded')"/>
-				<x-text-input id="xp_awarded" type="number" class="block mt-1 w-full" name="xp_awarded" :value="old('xp_awarded', $questLog->xp_awarded)" required/>
+				<x-text-input id="xp_awarded" type="number" class="block mt-1 w-full" name="xp_awarded" :value="old('xp_awarded', $questLog->xp_awarded)" min="0" required/>
 				<x-input-error :messages="$errors->get('xp_awarded')" class="mt-2"/>
 			</div>
 
 			<div class="mb-4">
 				<label for="xp_bonus" class="block text-gray-700 text-sm font-bold mb-2">Bonus XP:</label>
-				<input type="number" name="xp_bonus" id="xp_bonus" class="form-input w-full" value="{{ old('xp_bonus', $questLog->xp_bonus) }}">
+				<input type="number" name="xp_bonus" id="xp_bonus" class="form-input w-full" value="{{ old('xp_bonus', $questLog->xp_bonus) }}" min="0">
 				@error('xp_bonus')
 				<p class="text-red-500 text-xs italic mt-1">{{ $message }}</p>
 				@enderror
