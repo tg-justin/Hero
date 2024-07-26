db3b7822d4b66c2e9f1caa9760763d466cc1ccf6
Jeff <jeff@tabletopgaymers.org>
2024-07-25 22:17:16 -0500

    fixed Justin's broke ass web routes

--- routes/web.php
+++ routes/web.php
@@ -124,7 +124,6 @@ Route::middleware('auth')->group(function()
 		Route::post('/quests/{quest}/accept', [QuestController::class, 'accept'])->name('quests.accept');
 		Route::get('/quests/{quest}/duplicate', [QuestController::class, 'duplicate'])->name('quests.duplicate');
 		Route::get('/quests/{quest}/confirm-delete', [QuestController::class, 'destroy'])->name('quests.confirm-delete');
-		Route::get('/quests/{quest}/quest-logs', [QuestLogController::class, 'indexForQuest'])->name('quests.quest-logs');
 
 		Route::get('/quest-log', [QuestLogController::class, 'index'])->name('quest-log.index');
 
@@ -151,6 +150,8 @@ Route::middleware('auth')->group(function()
 			Route::get('/quest-logs/{questLog}/review', [QuestLogController::class, 'review'])->name('quest-logs.review');
 			Route::get('/quest-logs/{questLog}/edit', [QuestLogController::class, 'edit'])->name('quest-logs.edit');
 			Route::put('/quest-logs/{questLog}', [QuestLogController::class, 'update'])->name('quest-logs.update');
+
+			Route::get('/quests/{quest}/quest-logs', [QuestLogController::class, 'indexForQuest'])->name('quests.quest-logs');
 		});
 
 		/*******************************************************************************************
