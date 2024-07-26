fcbba71b8b5657ba9e155593c4d56487c317d521
Jeff <jeff@tabletopgaymers.org>
2024-07-25 22:05:35 -0500

    reworked the Show Quest to include more details after they've completed the quest.

--- resources/views/quests/show.blade.php
+++ resources/views/quests/show.blade.php
@@ -20,81 +20,86 @@
 			<div class="content-primary md:order-last">
 
 				<div class="bg-white px-3 py-1 rounded-md shadow border border-seance-600">
-					<h2 class="stat-header">Quest Details</h2>
+					<h2 class="stat-header mt-1">Quest Details</h2>
+					<table class="stat-block">
+						<tr>
+							<th>Level:</th>
+							<td>{{ $quest->min_level }}</td>
+						</tr>
 
-					<div class="flex pb-2">
-						<span class="font-bold w-24">Level:</span>
-						<span class="flex-1">{{ $quest->min_level }}</span>
-					</div>
-
-					@if ($questCompleted || $questAccepted)
-						<div class="flex pb-2">
-							<span class="font-bold w-24">Accepted:</span>
-							<span class="flex-1"><x-date-user-time-zone :value="$questLog->accepted_at"/></span>
-						</div>
-					@endif
-
-					@if($questCompleted)
-						<div class="flex pb-2">
-							<span class="font-bold w-24">Completed:</span>
-							<span class="flex-1"><x-date-user-time-zone :value="$questLog->completed_at"/></span>
-						</div>
-
-						<div class="flex pb-2">
-							<span class="font-bold w-24">XP Awarded:</span>
-							<span class="flex-1">
-								{{ $questLog->xp_awarded + $questLog->xp_bonus }}
-								@if($questLog->xp_bonus > 0)
-									({{ $questLog->xp_awarded }} + {{ $questLog->xp_bonus }} bonus)
-								@endif
-							</span>
-						</div>
-
-					@else
-						<div class="flex pb-2">
-							<span class="font-bold w-24">XP Award:</span>
-							<span class="flex-1">{{ $quest->xp }}</span>
-						</div>
-
-						@if ($quest->bonus_xp_text)
-							<div class="pb-2">
-								<span class="font-bold">Bonus XP:</span>
-								<span>{{ strip_tags($quest->bonus_xp_text) }}</span>
-							</div>
+						@if($questCompleted || $questAccepted)
+							<tr>
+								<th>Accepted:</th>
+								<td>
+									<x-date-user-time-zone :value="$questLog->accepted_at"/>
+								</td>
+							</tr>
 						@endif
-					@endif
 
-					@if(FALSE)
-						<div class="flex pb-2">
-							<span class="font-bold w-24">Category:</span>
-							<span class="flex-1">{{ $quest->category->name }}</span>
-						</div>
-					@endif
+						@if($questCompleted)
+							<tr>
+								<th>Completed:</th>
+								<td>
+									<x-date-user-time-zone :value="$questLog->completed_at"/>
+								</td>
+							</tr>
+							<tr>
+								<th>XP Awarded:</th>
+								<td>
+									{{ $questLog->xp_awarded + $questLog->xp_bonus }}
+									@if($questLog->xp_bonus > 0)
+										({{ $questLog->xp_awarded }} + {{ $questLog->xp_bonus }} bonus)
+									@endif
+								</td>
+							</tr>
+						@else
+							<tr>
+								<th>XP Award:</th>
+								<td>{{ $quest->xp }}</td>
+							</tr>
+							@if ($quest->bonus_xp_text)
+								<tr>
+									<th>Bonus XP:</th>
+									<td>{{ strip_tags($quest->bonus_xp_text) }}</td>
+								</tr>
+							@endif
+						@endif
 
-					@if ($quest->campaign)
-						<div class="flex pb-2">
-							<span class="font-bold w-24">Campaign:</span>
-							<span class="flex-1">{{ $quest->campaign->title }}</span>
-						</div>
-					@endif
+						@if(FALSE)
+							<tr>
+								<th>Category:</th>
+								<td>{{ $quest->category->name }}</td>
+							</tr>
+						@endif
 
-					@if ($quest->repeatable)
-						<div class="flex pb-2">
-							<span class="font-bold w-24">Repeatable:</span>
-							<span class="flex-1">{{ $quest->repeatable }}</span>
-						</div>
-					@endif
+						@if ($quest->campaign)
+							<tr>
+								<th>Campaign:</th>
+								<td>{{ $quest->campaign->title }}</td>
+							</tr>
+						@endif
 
-					@if($isEditor && $quest->notify_email != NULL)
-						<div class="flex pb-2">
-							<span class="font-bold w-24">Notify:</span>
-							<span class="flex-1">{{ $quest->notify_email }}</span>
-						</div>
-					@endif
+						@if ($quest->repeatable)
+							<tr>
+								<th>Repeatable:</th>
+								<td>{{ $quest->repeatable }}</td>
+							</tr>
+						@endif
+
+						@if($isEditor && $quest->notify_email != NULL)
+							<tr>
+								<th>Notify:</th>
+								<td>
+									{{ $quest->notify_email }}
+								</td>
+							</tr>
+						@endif
+					</table>
 
 					@if($quest->files->count() > 0)
 						<h2 class="stat-header mb-0">Attachments</h2>
 						<ul class="mt-0">
+
 							@foreach($quest->files as $file)
 								@php
 									$extension = strtoupper(pathinfo($file->path, PATHINFO_EXTENSION));
@@ -105,6 +110,8 @@
 							@endforeach
 						</ul>
 					@endif
+
+
 				</div> {{-- STATS: END --}}
 
 				<div class="p-0"> {{-- BUTTONS: BEGIN --}}
@@ -157,22 +164,23 @@
 			<div class="content-secondary md:order-first"> {{-- LEFT_COLUMN: BEGIN --}}
 				<div class="bg-white p-4 rounded-md shadow-inner"> {{-- QUEST BODY: BEGIN --}}
 					@if ($questCompleted)
-						{!! $quest->complete_text !!}
-						<div class="text-2xl font-bold text-seance-900 bg-gray-300 py-1 my-2 text-center">Quest Info</div>
-						{!! $quest->intro_text !!}
-						<hr class="my-4">
-						{!! $quest->accept_text !!}
-						<hr class="my-4">
-						{!! $quest->directions_text !!}
+						<div class="text-2xl font-bold text-seance-900 bg-gray-300 py-1 my-2 text-center">Quest Complete!</div>
+						<x-default-value :escape='FALSE' :value="$quest->complete_text" default="<p>You have completed this quest!</p>"/>
 
-						{{--						http://hero.test/quest-log/39/complete --}}
-						<div class="text-2xl font-bold text-seance-900 bg-gray-300 py-1 my-2 text-center">Your Submission</div>
-						<h3>Feedback</h3>
+						<div class="flex justify-between items-center">
+							<h3>Your Feedback</h3>
+							<span><a href="{{ route('quest-log.complete-form', $questLog) }}" class="tg-button-gray">Edit</a></span>
+						</div>
 						<div class="bg-gray-50 px-2 py-0 rounded-md border border-gray-500 mb-4 ml-8">
 							<x-default-value :escape='FALSE' :value="$questLog->feedback" :default="'<p><em>no feedback provided</em></p>'"/>
 						</div>
 
-						<h3>File Uploads</h3>
+
+
+						<div class="flex justify-between items-center">
+							<h3>Your Uploads</h3>
+							<span><a href="{{ route('quest-log.complete-form', $questLog) }}" class="tg-button-gray">Edit</a></span>
+						</div>
 						<ul class="mt-0">
 							@if($questLog->files->count() > 0)
 								@foreach($questLog->files as $file)
@@ -188,23 +196,39 @@
 							@endif
 						</ul>
 
-						<div class="mx-auto">
-							<a href="{{ route('quest-log.complete-form', $questLog) }}" class="tg-button-green">Complete Quest</a>
-						</div>
+						@if($questLog->reviewed_at != NULL)
+							<div class="flex justify-between items-center">
+								<h3>Reviewer Comments</h3>
+								@if ($isEditor)
+									<span><a href="{{ route('quest-logs.review', $questLog) }}" class="tg-button-gray">Edit</a></span>
+								@else
+									<span>&nbsp;</span>
+								@endif
+							</div>
+							<div class="bg-gray-50 px-2 py-0 rounded-md border border-gray-500 mb-4 ml-8">
+								<x-default-value :escape='FALSE' :value="$questLog->reviewer_message" :default="'<p><em>no comments provided</em></p>'"/>
+							</div>
+						@endif
+
+						<div class="text-2xl font-bold text-seance-900 bg-gray-300 py-1 my-2 text-center">Quest Info</div>
+
+					@endif  {{-- COMPLETED QUEST DETAILS: END --}}
 
 
-					@else
-						{!! $quest->intro_text !!}
-						@if (!$questLog || $isEditor)
-							{!! $quest->accept_text !!}
-						@endif
-						@if ($questAccepted || $isEditor)
-							{!! $quest->directions_text !!}
-						@endif
-						@if ($questCompleted || $isEditor)
-							{!! $quest->complete_text !!}
-						@endif
+					{!! $quest->intro_text !!}
+
+					@if (!$questLog || $isEditor || $questCompleted)
+						{!! $quest->accept_text !!}
 					@endif
+
+					@if ($questAccepted || $isEditor || $questCompleted)
+						{!! $quest->directions_text !!}
+					@endif
+
+					@if ($questCompleted || $isEditor || $questCompleted)
+						{!! $quest->complete_text !!}
+					@endif
+
 				</div>
 			</div>
 
